f225749ebe7ebbd24b62a2fcdc0bdb6bf0c575d5
Alan Somers <asomers@gmail.com>
2021-09-26 23:06:47 -0600

    Fix FUSE_LISTXATTR with size=0
    With size=0, the file system should reply with the size of the list and
    errno 0.  errno ERANGE should only be used if the request had a nonzero
    size that was too small.

--- src/raw/session.rs
+++ src/raw/session.rs
@@ -2349,7 +2349,7 @@ impl<'a, FS: Filesystem + Send + Sync + 'static> Session<'a, FS> {
 
                     let out_header = fuse_out_header {
                         len: (FUSE_OUT_HEADER_SIZE + FUSE_GETXATTR_OUT_SIZE) as u32,
-                        error: libc::ERANGE,
+                        error: 0,
                         unique: request.unique,
                     };
 
